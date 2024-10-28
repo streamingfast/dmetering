d11d4ec85e0507b83a2b131e7641b41359294908
colindickson <colin.dickson@gmail.com>
2024-10-28 14:30:59 -0400

    rename to OutputModuleHash

--- metering.go
+++ metering.go
@@ -13,9 +13,9 @@ import (
 )
 
 type Event struct {
-	Endpoint   string             `json:"endpoint"`
-	Metrics    map[string]float64 `json:"metrics,omitempty"`
-	OutputHash string             `json:"output_hash,omitempty"`
+	Endpoint         string             `json:"endpoint"`
+	Metrics          map[string]float64 `json:"metrics,omitempty"`
+	OutputModuleHash string             `json:"output_module_hash,omitempty"` //substreams-only field
 
 	UserID    string `json:"user_id"`
 	ApiKeyID  string `json:"api_key_id"`
